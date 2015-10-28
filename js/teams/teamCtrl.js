28bf558f06938397d73c3dbfe05314abaeaff170
jordangunther <jordankgunther@gmail.com>
2015-10-27 23:17:12 -0600

    nbaRoutes error connecting to the module. Almost finsihed

--- js/teams/teamCtrl.js
+++ js/teams/teamCtrl.js
@@ -1,5 +1,30 @@
 var app = angular.module('nbaRoutes');
 
-app.controller('teamCtrl', function($scope, $routeParams, teamService){
-
+app.controller('teamCtrl', function(teamData, $scope, $routeParams, teamService){
+	$scope.newGame = {};
+	$scope.showNewGameForm = false;
+	$scope.toggleNewGameForm = function(){
+		if($scope.showNewGameForm === false) {
+			$scope.showNewGameForm = true;
+		}
+		if($routeParams.team === 'utahjazz') {
+			$scope.homeTeam = 'Utah Jazz';
+			$scope.logPath = 'images/jazz-logo.png';
+		} else if ($routeParams.team === 'losangeleslakers') {
+			$scope.homeTeam = 'Los Angeles Lakers';
+			$scope.logPath = 'images/lakers-logo.png';
+		} else if ($routeParams.team === 'miamiheat') {
+			$scope.homeTeam = 'Miami Heat';
+			$scope.logPath = 'images/heat-logo.png';
+		}
+	}
+	$scope.submitGame = function(){
+		$scope.newGame.homeTeam = $scope.homeTeam.split(' ').join('').toLowerCase();
+		teamService.addNewGame($scope,newGame).then(function(res){
+			teamService.getTeamData($scope.newGame.homeTeam).then(function(res){
+				$scope.newGame = {};
+				$scope.ShowNewGameForm = false;
+			})
+		})
+	}
 });
--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -1,5 +1,40 @@
 var app = angular.module('nbaRoutes');
 
 app.service('teamService', function($http, $q){
-
+	this.addNewGame = function(gameObj) {
+		var url = 'https://api.parse.com/1/classes/' + gameObj.homeTeam;
+		if (parseInt(gameObj.homeTeamScore) > parseInt(gameObj.opponentScore)) {
+			gameObj.won = true;
+		} else {
+			gameObj.won = false;
+		}
+		return $http = {
+			method: 'POST',
+			url: url,
+			data: gameObj
+		}
+	}
+	this.getTeamData = function(team) {
+		var defer = $q.defer();
+		var url = 'https://api.parse.com/1/classes/' + team;
+		$http = {
+			method: 'GET',
+			url: url,
+		}.then(function(data){
+			var results = data.data.results;
+			var wins = 0;
+			var losses = 0;
+			for (var i = 0; i < results.length; i++){
+				if(results.won === true){
+					wins++;
+				} else if (results.won != true) {
+					losses++;
+				} 
+			}
+			results.win = wins;
+			results.losses = losses;
+			defer.resolve(results);
+		})
+		return defer.promise;
+	}
 });
